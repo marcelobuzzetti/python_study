fc0f8079fac9ffba10d87c7d028b41f071fbec67
Marcelo Buzzetti <marcelobuzzetti@gmail.com>
2024-12-28 10:31:36 -0300

    bing search

--- beatifulsoap/main.py
+++ beatifulsoap/main.py
@@ -4,15 +4,20 @@ from bs4 import BeautifulSoup
 def main():
     search = input("Enter search term: ")
     params = {"q": search, "oq": search}
-    r = requests.get("http://google.com/search", params=params)
+    r = requests.get("http://bing.com/search", params=params)
     # Parse the HTML content using BeautifulSoup
     soup = BeautifulSoup(r.text, "html.parser")
     # Extract the title of the webpage
-    title = soup.title.text
-    print("Page Title:", title)
-    links = soup.find_all("a") # Find all elements with the tag <a>
-    for link in links:
-        print("Link:", link.get("href"), "Text:", link.string)
+    results = soup.find("ol", {"id": "b_results"})
+    links = results.find_all("li", {"class": "b_algo"})
+    for item in links:
+        a_tag = item.find("a")
+        item_text = a_tag.get_text()
+        item_href = a_tag["href"]
+
+        if item_text and item_href:
+            print(item_text)
+            print(item_href)
 
 if __name__ == '__main__':
     main()
