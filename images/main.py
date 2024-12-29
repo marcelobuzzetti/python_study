c02f0327a14ed4cb796327b909bf2b1bbd92b164
Marcelo Buzzetti <marcelobuzzetti@gmail.com>
2024-12-29 10:41:54 -0300

    improve search

--- images/main.py
+++ images/main.py
@@ -5,9 +5,17 @@ from io import BytesIO
 from urllib.parse import unquote
 import json
 from datetime import datetime
+import os
 
 def main():
+    """
+    Buscando e salvando imagens do Bing
+    """
+    timestamp = datetime.now().strftime("%Y%m%d%H%M%S")
     search = input("Enter search term: ")
+    folder = f"{timestamp}_{search}"
+    if not os.path.exists(folder):
+        os.makedirs(folder)
     params = {"q": search}
     r = requests.get("http://bing.com/images/search", params=params)
     soup = BeautifulSoup(r.text, "html.parser")
@@ -25,8 +33,8 @@ def main():
             decoded_url = unquote(split)
             print(decoded_url)
             # pegando a imagem
-            img_obj = requests.get(decoded_url)
             try:
+                img_obj = requests.get(decoded_url)
                 img = Image.open(BytesIO(img_obj.content))
                 if img:
                     # Pegadno o atributo m que contem todos os dados do arquivo
@@ -39,14 +47,20 @@ def main():
                         if t_value:
                             # removendo espaços e limitando o tamanho a 10 caracteres
                             title = t_value.replace(" ", "").replace(".","")[:10]
+                    # Verificando se o arquivo já existe
+                    current_time = datetime.now().strftime("%Y%m%d%H%M%S")
                     if title:
-                        img.save(f"./scraped_images/{title}.{img.format}", img.format)
+                        file_path = f"./{folder}/{title}.{img.format}"
+                        if os.path.exists(file_path):
+                            img.save(f"./{folder}/{title}_{current_time}.{img.format}", img.format)                        
+                        else:
+                            img.save(f"./{folder}/{title}.{img.format}", img.format)
                     else:
-                        timestamp = datetime.now().strftime("%Y%m%d%H%M%S")
-                        print(timestamp)
-                        img.save(f"./scraped_images/{timestamp}.{img.format}", img.format)
+                        img.save(f"./{folder}/{current_time}.{img.format}", img.format)
             except Exception as e:
                 print(f"Could not save image: {e}")
+    # fazendo a busca ser reiniciada
+    main()
 
 if __name__ == '__main__':
     main()
