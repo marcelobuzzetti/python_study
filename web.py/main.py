27b3a96f653c57a47189fc64df486d5aa3721715
Marcelo Buzzetti <marcelobuzzetti@gmail.com>
2024-12-31 17:24:24 -0300

    templates

--- web.py/main.py
+++ web.py/main.py
@@ -2,15 +2,18 @@ import web
 
 # Urls accept regex
 urls = (
-    '/(.*)', 'index'
+    '/(.*)/(.*)', 'index'
 )
+# define templates
+render = web.template.render("resources/")
 
 # Define app
 app = web.application(urls, globals())
 
 class index:
-    def GET(self, name):
-        return f"Hello {name}!"
+    def GET(self, name, last_name):
+        # return f"Hello {name}!"
+        return render.main(name, last_name)
 
 if __name__ == "__main__":
     app.run()
